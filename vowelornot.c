b47f0253e899eec567901bef40af018610235134
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-20 10:53:19 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+scanf("%c",&a);
+if(a!='a'&&a!='e'&&a!='i'&&a!='o'&&a!='u')
+{
+printf("consonants");
+}
+else
+{
+printf("vowels");
+}
+getch();
+}
