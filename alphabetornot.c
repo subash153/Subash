fb6f31aa0753e894c133751794d0e5dd0768e48c
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-20 20:04:31 +0530

    Create alphabetornot.c

---
+++ alphabetornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char i;
+scanf("%c",&a);
+if((i=>'a'&&i<='z')||(i>='A'&&i<='Z'))
+{
+printf("alphabet");
+}
+else
+{
+printf("not alphabet");
+}
+getch();
+}
