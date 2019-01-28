cf638bd3af8b4d144fc0f4f705153d8b2199fa31
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-28 20:12:57 +0530

    Create numberrange.c

---
+++ numberrange.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,i,k;
+clrscr();
+scanf("%d%d%d",&k,&n,&m);
+if((k>n)&&(k<m))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
