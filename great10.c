853b7939fdb22480810c8785a97385eb9db30b2e
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 15:54:27 +0530

    Create great10.c

---
+++ great10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,c=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+c=a[0];
+for(j=0;j<n;j++)
+{
+if(a[k]>c)
+{
+c=a[k];
+}
+}
+printf("%d",c);
+}
