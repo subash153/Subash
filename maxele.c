557dc5e24d24e6a1cd8c825a0cec10a871226d04
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 15:48:11 +0530

    Create maxele.c

---
+++ maxele.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i,c=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=0;j<n;j++)
+{
+if(a[k]>c)
+{
+c=a[k];
+}
+}
+printf("%d",c);
+}
