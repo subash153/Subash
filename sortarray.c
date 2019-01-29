f975bf6b9a8d6d0ea5fa15a53d7e46c5e43532bb
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 16:01:12 +0530

    Create sortarray.c

---
+++ sortarray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,a,n,num[30];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(num[i]>num[30])
+{
+a=num[i];
+num[i]=num[j];
+num[j]=a;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d\n",num[i]);
+}
