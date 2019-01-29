940cc04187b85d18798064326d7446b48d88b4ac
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 11:23:45 +0530

    Create powertwo.c

---
+++ powertwo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,f=0;
+scanf("%d",&n);
+if(n%2==0)
+{
+f=1;
+}
+if(f==1)
+{
+printf("power of 2");
+}
+else
+{
+printf("not a power of 2");
+}
+getch();
+}
