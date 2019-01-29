154d79a396488e711c0910542f708447634c2c07
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 10:28:18 +0530

    Create evendisplay.c

---
+++ evendisplay.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,q;
+scanf("%d%d",&n,&q);
+while(q<=100000)
+{
+for(i=n+1;i<q;i++)
+{
+if(i%2==0)
+{
+printf("%d",i);
+}
+}
+}
+getch();
+}
