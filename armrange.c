63de60d4217c0793802f48c796ee478b90545bb2
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 10:43:58 +0530

    Create armrange.c

---
+++ armrange.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,m,temp1,temp2,c=1,sum=0,l=0,rem;
+clrscr();
+scanf("%d%d",&a,&m);
+for(i=a+1;i<m;i++)
+{
+temp1=a;
+temp2=a;
+while(a>0)
+{
+a=a/10;
+l++;
+}
+while(temp1>0)
+{
+c=1;
+rem=temp1%10;
+for(i=0;i<l;i++)
+{
+c=c*rem;
+}
+sum=sum+c;
+temp1=temp1/10;
+}}
+if(sum==temp2)
+{
+printf("armstrong");
+}
+else
+{
+printf("not armstrong");
+}
+getch();
+}
