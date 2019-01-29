2f6661aeebe2bbb45e618d165e574299fe5bf17d
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 12:04:23 +0530

    Create spacecount.c

---
+++ spacecount.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[30];
+int n=0;
+scanf("%[^\n]",&s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]==' ')
+{
+n++;
+}
+}
+printf("%d",n);
+getch();
+}
