aa3f79949b5d19b8941d5b2d5659cbfb8c804913
subash153 <46838524+subash153@users.noreply.github.com>
2019-01-29 15:45:37 +0530

    Create largesmall.c

---
+++ largesmall.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i,c=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=0;j<n;j++)
+{
+if(a[k]>c)
+{
+c=a[k];}
+}
+printf("%d",c);
+}
+c=a[0];
+for(k=0;k<n;k++)
+{
+if(a[k]>c)
+{
+c=a[k];
+}
+}
+getch();
+}
