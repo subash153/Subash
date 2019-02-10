52e849c688453aedfef0b46a0e5776bc391bd3f4
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 21:13:28 +0530

    Create oddprint.c

---
+++ oddprint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[15],i,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+if(a[i]%2==0)
+{
+break;
+}
+else
+{
+printf("%d",a[i]);
+}
+getch();
+}
