f4104127e41bb85625624650626a71a75a382aab
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 17:41:47 +0530

    Create minten.c

---
+++ minten.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,min,size,loc=0;
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&a[i]);
+min=a[0];
+for(j=0;j<size;j++)
+{
+if(a[c]<min)
+{
+min=a[c];
+loc=c+1;
+}
+}
+getch();
+}
