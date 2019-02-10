8bb00ce3244ab777b42e90234140dc88a5e43c04
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 14:52:52 +0530

    Create numpre.c

---
+++ numpre.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,m,i;
+scanf("%d%d",&n,&m);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+if(a[i]==m)
+{
+printf("yes");
+}
+getch();
+}
