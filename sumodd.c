4ad14442e1b5e47b7596bb5a2d2920bf4f525d7f
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 17:44:48 +0530

    Create sumodd.c

---
+++ sumodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,h;
+scanf("%d%d",&n,&m);
+h=n+m;
+if(h%2==1)
+{
+printf("odd number");
+}
+else
+{
+printf("even number");
+}
+getch();
+}
