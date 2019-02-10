3f4c5732cfbcf670c6c29e1a99e002f62fc94a16
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 14:24:16 +0530

    Create numrepeat.c

---
+++ numrepeat.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,i,c=0;
+clrscr();
+scanf("%d%d",&n,&m);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&n);
+}
+if(n==m)
+{
+c++;
+}
+printf("%d",c);
+getch();
+}
