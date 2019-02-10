342f351efcc2e1a03c780c9e40cdd9d772f17da3
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 19:48:13 +0530

    Create vowpre.c

---
+++ vowpre.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[10];
+clrscr();
+for(i=0;a[i]!='\0';i++)
+{
+scanf("%d",&a[i]);
+}
+if((a[i]=='a')&&(a[i]=='e')&&(a[i]=='i')&&(a[i]=='o')&&(a[i]=='u'))
+{
+printf("vowel present");
+}
+else
+{
+printf("not present");
+}
+getch();
+}
