47c56d51ac6ecbefea13e86adc0334ee97189361
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 21:08:32 +0530

    Create mulper.c

---
+++ mulper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(a = 0; a <= n; a++)
+    {
+        if (n == a * a)
+        {
+            printf("YES");
+            return 0;
+        }
+    }
+    printf("NO");
+    return 0;
+}
