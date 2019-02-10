247f71b4dd8c16227c83c19af7b54c6af56e21e0
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-10 18:32:30 +0530

    Create nearten.c

---
+++ nearten.c
@@ -0,0 +1,16 @@
+#include<stdiio.h>
+#include<conio.h>
+int main()
+{
+int round(int n);
+{
+int a=(n/10)*10;
+int b=a+10;
+return(n-a>b-n)?b:a;
+}
+public static void main()
+{
+int n=4722;
+console.writeline(round(n));
+}
+}
