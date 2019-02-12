02ab134df3743c89c4c113644204114c60e9ec42
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-12 13:04:51 +0530

    Update Diff.c

--- Diff.c
+++ Diff.c
@@ -7,8 +7,8 @@ scanf("%d%d%d%d%d%d",&kab,&opp,&kab1,&opp1,&kab2,&opp2);
 line1=kab-opp;
 line2=kab1-opp1;
 line3=kab2-opp2;
-printf("difference in line 1 is %d",line1);
-printf("difference in line 2 is %d",line2);
-printf("difference in line 3 is %d",line3);
+printf("diff in line 1 is %d\n",line1);
+printf("diff in line 2 is %d\n",line2);
+printf("diff in line 3 is %d",line3);
 getch();
 }
