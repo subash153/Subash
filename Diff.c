9bdd71f538aa37e66d34b7cc1b79245cda52fa6a
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-12 13:06:17 +0530

    Update Diff.c

--- Diff.c
+++ Diff.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+int main()
 {
 int kab,opp,kab1,opp1,kab2,opp2,line1,line2,line3;
 scanf("%d%d%d%d%d%d",&kab,&opp,&kab1,&opp1,&kab2,&opp2);
@@ -10,5 +10,5 @@ line3=kab2-opp2;
 printf("diff in line 1 is %d\n",line1);
 printf("diff in line 2 is %d\n",line2);
 printf("diff in line 3 is %d",line3);
-getch();
+return 0;
 }
