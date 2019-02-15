cc536697be7387b68ff71a571660c71da12b461e
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-15 16:26:41 +0530

    Create div2.c

---
+++ div2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,n1,m1;
+scanf("%d%d",&n,&m);
+if((n%2==0)&&(m%2==0))
+{
+n1=(n%2==0);
+m1=(m%2==0);
+printf("%d%d",n1,m1);
+}
+getch();
+}
