90f8c4f44f39bcd61a37dc2db3cba767c394c98c
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-17 17:39:39 +0530

    Create 105.c

---
+++ 105.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=64,n;
+    clrscr();
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    printf("%c\n",a+n);
+    return 0;
+}
