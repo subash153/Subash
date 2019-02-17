cb5b44e53df26898afbf079244abb987ca00a3cf
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-17 17:36:49 +0530

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value");
+scanf("%d",&a);
+a=a+64;
+printf("Corresponding alphabet is %c",a);
+getch();
+}
