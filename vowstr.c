cde8c6cce5760e993e80de724052a4a84418e9b1
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-19 15:47:24 +0530

    Create vowstr.c

---
+++ vowstr.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+char s[20],r[20];
+clrscr();
+scanf("%d",&n);
+for(i=0;s[i]!='\0';i++)
+{
+scanf("%c",&s[i]);
+}
+for(i=0;s[i]!='\0';i++)
+{
+if((s[i]=='a)||(s[i]=='e')||(s[i]=='i')||(s[i]=='o')||(s[i]=='u'))
+{
+s[i]=='\0';
+}
+if(s[i]!='\0')
+{
+printf("%s",s[i]);
+}
+}
+for(i=0;s[i]='\0';i++)
+{
+r[h]=s[i];
+}
+for(i=h-1;i>=0;i--)
+{
+printf("%c",r[h]);
+}
+getch();
+}
