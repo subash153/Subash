e016c1d573c4955631be89c32b7848a14ca8f45f
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-19 16:26:41 +0530

    Create ksmall.c

---
+++ ksmall.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i,c=0,h;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",&k);
+while(h>0)
+{
+c=a[0];
+for(k=0;k<n;k++)
+{
+if(a[k]>c)
+{
+c=a[k];
+}
+k='\0';
+}
+h--;
+}
+getch();
+}
