ae720eae81a7e117fdf09106572b868867cbe436
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-21 15:57:18 +0530

    Rename numpre.c to numpre1.c

---
+++ numpre1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num[10],m,i,n;
+scanf("%d%d",&n,&m);
+for(i=0;i<n;i++)
+{
+scanf("%d",&num[i]);
+}
+if(num[i]==m)
+{
+printf("yes");
+}
+getch();
+}
