142aa76dae1a8714380f55724202f9a47d15a849
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-22 16:01:43 +0530

    Create indexfind.c

---
+++ indexfind.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i;
+scanf("%d",&n);
+for(i=0;a[i]!='\0';i++)
+{
+scanf("%d",&a[i]);
+}
+if(a[i]==n)
+{
+printf("%d",i);
+}
+getch();
+}
