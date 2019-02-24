9ef15b5c94dd575da82db702f3ec81cb0fcf31c4
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-24 18:26:01 +0530

    Create set2-40.c

---
+++ set2-40.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+intcounterswitch()
+{
+int length;
+{
+    int result = 0; 
+ 
+
+    for (int x=1; x<=n; x++)
+        result += has4(x)? 1 : 0;
+ 
+    return result;
+}
