342eeab6a8150984195763b091fad6911f97b25a
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-24 11:34:21 +0530

    Create set2-57.c

---
+++ set2-57.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10],s;
+int i,c=0;
+scanf("%c",&s);
+for(i=0;i!='\0';i++)
+{
+scanf("%c%c",&a[i]);
+}
+if(a[i]==s)
+{
+c++;
+}
+getch();
+}
