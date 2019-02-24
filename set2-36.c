45be9c7af25801c1050e856bb55d03cc6e70bdb1
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-24 17:52:10 +0530

    Create set2-36.c

---
+++ set2-36.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n[10],m,i,k=0;
+scanf("%d",&m);
+for(i=0;i!='\0';i++)
+{
+scanf("%d",&n[i]);
+}
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==m)
+{
+k++;
+}
+}
+getch();
+}
