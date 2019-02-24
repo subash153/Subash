486d673b519f22b39165408ea2828d6eb8f2e9ec
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-24 19:00:22 +0530

    Create set2-24.c

---
+++ set2-24.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char str[30];
+    int n,i,flag=0;
+    scanf("%s",str);
+    n=strlen(str);
+    for(i=0;i<n;i++)
+    {
+        if(str[i]>='0' && str[i]<='9' )
+        {
+            flag=1;
+        }
+        else
+            break;
+    }
+    if(flag==1)
+    {
+        printf("yes");
+    }
+    else
+        printf("no");
+        getch();
+}
