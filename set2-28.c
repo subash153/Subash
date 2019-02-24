82fbb3ff9f837fd066ca044cee7fb31b33e1e2c1
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-24 18:30:22 +0530

    Create set2-28.c

---
+++ set2-28.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+	int n,i;
+	scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+	    if( i%2==0 && n%i==0)
+	    {
+	        printf("%d ",i);
+	    }
+	}
+	getch();
+}
