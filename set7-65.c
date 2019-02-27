2b5ef1868d0351053be018d21b2c27d459cecc97
subash153 <46838524+subash153@users.noreply.github.com>
2019-02-27 16:07:08 +0530

    Create set7-65.c

---
+++ set7-65.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[10],i,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    
+    }
+    for(k=0;k<n;k++)
+    {
+        if(a[k]<n)
+        {
+            b[k]=a[k];
+        
+        
+    printf("%d",b[k]);
+        }
+}
+    
+}
