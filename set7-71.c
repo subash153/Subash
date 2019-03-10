732d4048d9f4b7ce401d4522710c975b0a231208
subash153 <46838524+subash153@users.noreply.github.com>
2019-03-10 19:13:33 +0530

    Create set7-71.c

---
+++ set7-71.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<N;j++)
+{
+if(i<j)
+{
+printf("%d",j);
+}
+else
+{
+printf("%d",i);
+}
+}
+i+2;
+}
+getch();
+}
