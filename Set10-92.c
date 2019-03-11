70124469f67007a9be80be7e609665af3308f01d
subash153 <46838524+subash153@users.noreply.github.com>
2019-03-11 11:42:42 +0530

    Create Set10-92.c

---
+++ Set10-92.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem,base=1,dec=0;
+while(n>0)
+{
+rem=n%10;
+n=n/10;
+dec=dec+(r*base);
+base=base*2;
+}
+printf("%d",binary);
+getch();
+}
