372d412c909fa2ad962a96f44b08e0ac09e60b2d
subash153 <46838524+subash153@users.noreply.github.com>
2019-03-30 08:44:15 +0530

    Create Set12-120.c

---
+++ Set12-120.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int num,sum=0,sq,rem;
+scanf("%d",&num);
+while(num!=0)
+{
+rem=num%10;
+sq=rem*rem;
+sum=sum+sq;
+num=num/10;
+}
+printf("\n%d",sum);
+return 0
+}
