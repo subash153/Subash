4123d87ea443305ba0d1999fb47de834e4327af6
subash153 <46838524+subash153@users.noreply.github.com>
2019-03-31 19:35:52 +0530

    Create set17-165.c

---
+++ set17-165.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],i,f,k,p;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            f=1;
+        }
+        else if(a[i]>k)
+        {
+            f=0;
+            p=a[i];
+            break;
+        }
+    }
+    if(f==0)
+    {
+        printf("%d",p);
+    }
+    getch();
+}
