d0064826c2c834fb95d0e5d68a3ac91cad9840ed
subash153 <46838524+subash153@users.noreply.github.com>
2019-03-31 19:29:51 +0530

    Create set17-163.c

---
+++ set17-163.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,k,i=0,t=0,a[10000];
+ scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+       {
+           if(a[i]==k)
+           {
+               t++;
+               break;
+           }
+       }
+       if(t!=0)
+       printf("yes");
+       else
+       printf("no");
+       getch();
+}
